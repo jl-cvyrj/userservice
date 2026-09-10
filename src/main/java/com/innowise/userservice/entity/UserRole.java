89b8bbc437bf928e6ee88417cd13486d6c2cef68
jl-cvyrj <karolina.pavlovskaya9072@gmail.com>
2026-09-10 14:43:40 +0300

    feature: add SERVICE role for internal service-to-service auth

--- src/main/java/com/innowise/userservice/entity/UserRole.java
+++ src/main/java/com/innowise/userservice/entity/UserRole.java
@@ -4,7 +4,8 @@ import org.springframework.security.core.GrantedAuthority;
 
 public enum UserRole implements GrantedAuthority {
     ADMIN,
-    USER;
+    USER,
+    SERVICE;
 
     @Override
     public String getAuthority() {
